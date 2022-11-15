2338f652cc5720f9180a12cea97d925949ced02d
Nathan099 <82057687+Nathan099-dev@users.noreply.github.com>
2022-11-15 18:35:39 -0300

    modificação no script de cadastro do usuário

--- PROJETO_PESSOAL_1ADS/site/js/cadastro.js
+++ PROJETO_PESSOAL_1ADS/site/js/cadastro.js
@@ -1,4 +1,4 @@
-function cadastrar(pessoa){
+function cadastrar(){
     const pessoa = {
         nome: nome.value,
         idade: idade.value,
@@ -7,10 +7,38 @@ function cadastrar(pessoa){
         confirma_senha: confirmacao_senha.value,
     },
 
-    while (pessoa.value == null) {
-        window.alert('Campos inválidos');
-        pessoa.style.borderColor = 'red';
+    if (nome.value == null) {
+        window.alert('Campo inválido');
+        nome.style.borderColor = 'red';
+
+    }else if (idade.value == null) {
+        window.alert('Campo inválido');
+        idade.style.borderColor = 'red';
+
+    }else if (email.value == null) {
+        window.alert('Campo inválido');
+        email.style.borderColor = 'red';
+
+    }else if (email.indexOf('@').value == -1 || email.indexOf('.').value == -1) {
+        window.alert('Isso não é um email');
+        email.style.borderColor = 'red';
+
+    }else if (senha.value == null) {
+        window.alert('Campo inválido');
+        senha.style.borderColor = 'red';
+
+    }else if (senha.length < 8) {
+        window.alert('Sua senha precisa ter pelo menos oito caracteres')
+        senha.style.borderColor = 'red';
+
+    }else if (confirma_senha != senha) {
+        window.alert('As duas senhas cadastradas não coincidem');
+        confirma_senha.style.borderColor = 'red';
+
+    }else{
+        pessoa.style.borderColor = 'green'
     }
+
+    return cadastrar();
 }
 
-cadastrar();
