5d7bc4da45d8a9647c90d1af53aadd24850ad0db
Nathan099 <82057687+Nathan099-dev@users.noreply.github.com>
2022-11-15 13:05:21 -0300

    Modificaçao nas tabelas

--- PROJETO_PESSOAL_1ADS/site/banco_de_dados/site.sql
+++ PROJETO_PESSOAL_1ADS/site/banco_de_dados/site.sql
@@ -1,7 +1,7 @@
 create database iLoveRock;
 use iLoveRock;
 
--- Criando tabela usuário
+-- criando tablas
 
 create table usuario_cadastrado(
     id_usuario int primary key auto_increment;
@@ -11,9 +11,19 @@ create table usuario_cadastrado(
     sexo varchar(45)
 );
 
--- inserindo usuários
+CREATE TABLE subgêneros(
+    id_subgrenero int PRIMARY KEY auto_increment,
+    nome VARCHAR(40)
+);
 
-insert into usuario_cadastrado values
+CREATE TABLE bandas(
+    id_banda int PRIMARY KEY auto_increment,
+    nome VARCHAR(40)
+);
+
+-- inserindo dados nas tbelas
+
+INSERT INTO usuario_cadastrado VALUES
 (null, 'Douglas', 'Jen', 40, 'masculino'),
 (null, 'Nathan', 'Tonini' 23, 'masculino'),
 (null, 'Bruno', 'Tonini', 21, 'masculino'),
@@ -25,116 +35,24 @@ insert into usuario_cadastrado values
 (null, 'Bianca ', 'de Villa',19, 'feminino');
 (null, 'Nicolas', 'de Villa', 17, 'masculino');
 
--- Criando as tabelas
-
-CREATE TABLE banda_heavy_metal(
-id_banda_heavy_metal int PRIMARY KEY auto_increment,
-nome VARCHAR(40)
-);
-
-CREATE TABLE banda_hard_rock(
-id_banda_hard_rock int PRIMARY KEY auto_increment,
-nome VARCHAR(40)
-);
-
-CREATE TABLE banda_progressivo(
-id_banda_progressivo int PRIMARY KEY auto_increment,
-nome VARCHAR(40)
-);
-
-CREATE TABLE banda_metal_melódico(
-    id_banda_metal_melódico int PRIMARY KEY auto_increment,
-    nome VARCHAR(40)
-);
-
--- inserindo dados nas tabelas
-
-INSERT INTO banda_heavy_metal (id_banda_heavy_metal, nome) VALUES 
-(1, 'Guns and Roses'),
-(2, 'System of a Down'),
-(3, 'Slipknot'),
-(4, 'Iron Maiden'),
-(5, 'Black Sabath'),
-(6, 'Dio'),
-(7, 'Ozzy Osbourne'),
-(8, 'Angra'),
-(9, 'Nightwish'),
-(11, 'Judas Priest'),
-(12, 'Raimbow'),
-(13, 'Dream Theater'),
-(14, 'Metalica'),
-(15, 'Disturbed')
-(16, 'Rhapsody of Fire'),
-(17, 'Saxon'),
-(18, 'Shadowside'),
-(19, 'Queensriche'),
-(20, 'Savatage');
 
 
-INSERT INTO banda_hard_rock (id_banda_hard_rock, nome) VALUES 
-(1, 'Scorpions'),
-(2, 'Queen'),
-(3, 'Bon Jovi'),
-(4, 'Evanecence'),
-(5, 'Foo Fighters'),
-(6, 'Journey'),
-(7, 'Bad Company'),
-(8, 'Van Halen'),
-(9, 'Aerosmith'),
-(10, 'Alice Cooper'),
-(11, 'Alice in Chains'),
-(12, 'Uriah Heep'),
-(13, 'Led Zeppelin'),
-(14, 'Skillet'),
-(15, 'Heart'),
-(16, 'skid Row'),
-(17, 'Europe'),
-(18, 'Stryper'),
-(19, 'Twisted Siater'),
-(20, 'Whitesnake');
+INSERT INTO subgêneros VALUES
+(null, 'hard rock'),
+(null, 'heavy metal'),
+(null, 'Progressivo'),
+(null, 'metal melódico'),
+(null, 'soft rock');
 
 
-INSERT INTO banda_progressivo (id_banda_progressivo, nome) VALUES
-(1, 'Casa das Máquinas'),
-(2, 'Kansas'),
-(3, 'Rush'),
-(4, 'Marilion'),
-(5, 'Supertramp'),
-(6, 'Gênesis'),
-(7, 'Yes'),
-(8, 'Jethro Tull'),
-(9, 'Focus'),
-(10, 'Mutantes');
-
-INSERT INTO banda_metal_melódico (id_banda_metal_melódico, nome) VALUES
-(1, 'Heavenly'),
-(2, 'Nightwish'),
-(3, 'Angra'),
-(4, 'Shaman'),
-(5, 'Blind Guardian'),
-(6, 'DragonForce'),
-(7, 'Stratovarius'),
-(8, 'Manowar'),
-(9, 'Symphony X');
-(10, 'Kalidia');
-(11, 'Lunática');
-(12, 'Temperence');
-(13, 'Eluveitie');
-(14, 'Within Temptation');
-(15, 'Avantasia');
-(16, 'Viper');
-
--- mostrando os dados na tela
-
-SELECT * FROM usuario_cadastrado;
-SELECT * FROM banda_heavy_metal;
-SELECT * FROM banda_hard_rock;
-SELECT * FROM banda_progressivo;
-SELECT * FROM banda_metal_melódico;
-
-fk_usuario INT, 
-FOREIGN KEY (fk_usuario_cadastrado) REFERENCES usuario_cadastrado (id_usuario);
-
-
--- juntando tabelas
-
+INSERT into bandas VALUES 
+('Iron Maiden'),
+('Angra'),
+('Evannecence'),
+('Whitesnake'),
+('Casa das máquinas'),
+('Kansas'),
+('Nightwish'),
+('Kalidia'),
+('Phill Collins'),
+('Eagles');
