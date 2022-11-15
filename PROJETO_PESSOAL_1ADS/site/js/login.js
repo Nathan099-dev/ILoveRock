b5d7e19d832ce124933c397185b2a09a5a8c845f
Nathan099 <82057687+Nathan099-dev@users.noreply.github.com>
2022-11-15 16:20:58 -0300

    modificação script login

--- PROJETO_PESSOAL_1ADS/site/js/login.js
+++ PROJETO_PESSOAL_1ADS/site/js/login.js
@@ -1,22 +1,24 @@
 const email = document.getElementById('mail_login');
 const senha = document.getElementById('password_login');
-const btn = document.getElementById('btn_login');
+const btn = document.getAnimations('btn_login');
 
-btn.addEventListener('click', function login(){
+btn.addEventListenner('click', function login(){
     if (email.value == null) {
         window.alert('Campo inválido');
         email.style.borderColor = 'red';
 
-    }else if (email.indexOf('@') == -1 || email.indexOf('.') == -1) {
-        window.alert('Preencha um email válido');
-        email.style.borderColor = 'red';
-
-    }else if(senha.value == null){
-        window.alert('Digite sua senha');
-        senha.style.borderColor = 'red';
+    }else{
+        email.style.borderColor = none;
     }
 
+    if (senha.value == null) {
+        window.alert('Campo inválido');
+        senha.style.borderColor = 'red';
+
+    }else{
+        senha.borderColor = none;
+    }
 });
 
-login();
+return login();
 
