701a8835551f881d9ad124c5b48e20caba47aaa7
Nathan099 <82057687+Nathan099-dev@users.noreply.github.com>
2022-11-18 16:03:45 -0300

    modificação script

--- PROJETO_PESSOAL_1ADS/site/js/cadastro.js
+++ PROJETO_PESSOAL_1ADS/site/js/cadastro.js
@@ -1,24 +1,53 @@
-const lista_usuario = []
+function validar_campo_cadastro() {
+    var nome_usuario = input_nome.value
+    var email_usuario = input_email.value
+    var senha_usuario = input_senha.value
+    var repetir_senha = input_repetir_senha.value
+    var cpf = input_cpf.value
 
-function cadastrar(){
-    const novo_usuario = {
-        nome: nome.value,
-        idade: idade.value,
-        email: email.value,
-        senha: senha.value,
-        confirmar_sua_senha: confirmacao_senha.value,
-    }
-    lista_usuario.push(novo_usuario);
 
-    for(var novo_usuario_cadastrado; lista_usuario.length; novo_usuario_cadastrado++){
-        if (novo_usuario.value == null) {
-            window.alert('Campos inválidos');
-            novo_usuario.style.borderColor = 'red';
+    if (nome_usuario.length < 3) {
+        alert('Preencha este campo corretamente');
+        input_nome.style.borderColor = '#ff0000';
 
-        }else{
-            novo_usuario.style.borderColor = 'green'
-        }
     }
 
-    return cadastrar();
-}
+    else if (email_usuario.indexOf('@') == -1 || email_usuario.indexOf('.') == -1) {
+        alert('Preencha este campo corretamente');
+        input_email.style.borderColor = '#ff0000';
+
+    } else if (senha_usuario.length < 8) {
+        alert('Sua senha precisa ter  pelo menos oito caracteres');
+        input_senha.style.borderColor = '#ff0000';
+
+    } else if (repetir_senha.length == 0) {
+        input_repetir_senha.style.borderColor = 'rgb(255, 0, 0)';
+        alert('Insira sua senha novamente');
+
+    } else if (senha_usuario.length != repetir_senha.length) {
+        input_repetir_senha.style.borderColor = 'rgb(255, 0, 0)';
+        alert('Os campos não coincidem');
+
+    } else if (cpf == "") {
+        input_cpf.style.borderColor = 'rgb(255, 0, 0)';
+        alert('Informe seu CPF para continuar');
+
+    } else if (cpf.length < 11) {
+        alert('Campo inválido');
+        input_cpf.style.borderColor = '#ff0000';
+
+    }
+
+
+
+    else {
+        input_cpf.style.borderColor = 'rgb (0, 255, 0)';
+        input_email.style.borderColor = 'rgb (0, 255, 0)';
+        input_nome.style.borderColor = 'rgb (0, 255, 0)';
+        input_senha.style.borderColor = 'rgb (0, 255, 0)';
+        input_repetir_senha.style.borderColor = 'rgb (0, 255, 0)';
+        alert('Usuário Cadastrado com sucesso!');
+
+    }
+
+};
