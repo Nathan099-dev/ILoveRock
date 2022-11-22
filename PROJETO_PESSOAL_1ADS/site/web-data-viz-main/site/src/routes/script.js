d708c144bef7029b1dbb5ebd192d3d3f01a06daf
Nathan099-dev <tonininathan8@gmail.com>
2022-11-22 14:59:30 -0300

    modificações gerais

---
+++ PROJETO_PESSOAL_1ADS/site/web-data-viz-main/site/src/routes/script.js
@@ -0,0 +1,12 @@
+const botao = document.getElementById('btn');
+botao.onclick = function voltar_ao_topo(){
+	top: 0
+	left: 0
+	behavior: 'smooth'
+};
+
+voltar_ao_topo();
+
+function voltar(){
+	window.location.replace('../index.html');
+}
