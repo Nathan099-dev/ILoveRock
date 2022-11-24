5b31d2580d24389fb349b7b939309362f38e2ec9
Nathan099 <82057687+Nathan099-dev@users.noreply.github.com>
2022-11-24 20:31:22 -0300

    correção script

--- PROJETO_PESSOAL_1ADS/site/web-data-viz-main/site/public/js/cadastro.js
+++ PROJETO_PESSOAL_1ADS/site/web-data-viz-main/site/public/js/cadastro.js
@@ -2,13 +2,12 @@ const nome = document.getElementById('input_nome');
 const idade = document.getElementById('input_idade');
 const email = document.getElementById('input_email');
 const senha = document.getElementById('input_senha');
-const confirmar_senha = document.getElementById('input_confirma_senha');
+const confirma_senha = document.getElementById('input_confirma_senha');
 
-const btn = document.querySelector('button');
-
-btn.onclick = function cadastrar(){
-    if (nome.value = '') {
-        nome.innerHTML = 'Campo inválido'
-        nome.style.borderColor = 'red'
+const botao = document.querySelector('button');
+botao.onclick = function cadastrar(nome, idade, email, senha, confirmar_senha){
+    if (nome.value == '') {
+        window.alert('Campo inválido');
+        nome.style.borderColor = 'red';
     }
 }
