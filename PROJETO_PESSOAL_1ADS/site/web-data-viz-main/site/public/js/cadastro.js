e58db14ef9f200d4d1f63c6975c0be41f8c49164
Nathan099-dev <tonininathan8@gmail.com>
2022-11-25 14:32:18 -0300

    modificaçõesn a estrutura do site

--- PROJETO_PESSOAL_1ADS/site/web-data-viz-main/site/public/js/cadastro.js
+++ PROJETO_PESSOAL_1ADS/site/web-data-viz-main/site/public/js/cadastro.js
@@ -5,9 +5,41 @@ const senha = document.getElementById('input_senha');
 const confirma_senha = document.getElementById('input_confirma_senha');
 
 const botao = document.querySelector('button');
-botao.onclick = function cadastrar(nome, idade, email, senha, confirmar_senha){
-    if (nome.value == '') {
-        window.alert('Campo inválido');
-        nome.style.borderColor = 'red';
+
+function cadastrar(){
+    var nome_usuario = input_nome.value;
+    var idade_usuario = input_idade.value;
+    var email_usuario = input_email.value;
+    var senha_usuario = input_senha.value;
+    var confirmar_senha = input_confirma_senha.value;
+
+    if (nome_usuario == '') {
+        alert('Campo  nome, inválido');
+       
+        
+    }else if (idade_usuario == '') {
+        alert('Campo idade, inválido');
+       
+
+    }else if (email_usuario == '') {
+        alert('Campo email, inválido');
+       
+
+    }else if (email_usuario.indexOf('@').value == -1 || email_usuario.indexOf('.').value == -1) {
+        alert('Campo email, inválido');
+
+    }else if (senha_usuario == '') {
+        alert('Campo senha, inválido');
+        
+
+    }else if (confirmar_senha == '') {
+        alert('Campo de confirmação de senha, inválido')
+
+        
+    }else if (confirmar_senha != senha_usuario) {
+        alert('Suas senhas não coincidem');
+
     }
+
+    <li><a href="./login.html">Login</a></li>
 }
