f57de4058aba33e385e6cfaa284742be345ef96d
Nathan099-dev <tonininathan8@gmail.com>
2022-12-03 17:24:40 -0300

    correção nos formulários

--- PROJETO_PESSOAL_1ADS/site/web-data-viz-main/site/public/js/login.js
+++ PROJETO_PESSOAL_1ADS/site/web-data-viz-main/site/public/js/login.js
@@ -1,8 +1,17 @@
 function logar(){
-    var email_usuario = input_email_login.value;
-    var senha = input_password_login.value;
+    const email = input_email_login.value;
+    const senha = input_password_login.value;
 
-    if (email_usuario == '') {
-        alert('Campo inválido');
+    if (email == '') {
+        window.alert('Campo de email, inválido');
+        email.style.borderColor = 'red';
+    }
+
+    else if (senha == '') {
+        window.alert('Campo de senha, inválido')
+    }
+
+    else {
+        window.location.replace('./dashboard.html');
     }
 }
