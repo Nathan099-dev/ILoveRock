9b14adbaa1d0977977f91bb379ab7cb9a36e515a
Nathan099-dev <tonininathan8@gmail.com>
2022-12-03 11:05:48 -0300

    alterações html e js

--- PROJETO_PESSOAL_1ADS/site/web-data-viz-main/site/public/js/login.js
+++ PROJETO_PESSOAL_1ADS/site/web-data-viz-main/site/public/js/login.js
@@ -1,19 +1,16 @@
-const email = document.getElementById('mail_login');
-const senha = document.getElementById('password_login');
-const btn = document.getElementById('btn_login');
+function logar(){
+    const email = mail_login.value;
+    const senha = password_login.value;
 
-btn.addEventListener('click', function login(){
-    const email_usuario_login = email.value;
-    const senha_usuario_login = senha.value;
-
-    if (email_usuario_login == '') {
-        email_usuario_login.innerHTML = 'Campo inválido';
-        email.style.borderColor = 'red';
-
-    }else if (senha_usuario_login == '') {
-        senha_usuario_login.innerHTML = 'Campo inválido';
-        senha.borderColor = 'red';
+    if (email == '') {
+        window.alert('Campo incorreto');
     }
-})
 
-login();
+    if (senha == '') {
+        window.alert('Campo invaĺido');
+    }
+
+    else{
+        window.location.replace('./dashboard.html');
+    }
+}
